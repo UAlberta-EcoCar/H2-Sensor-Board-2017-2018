6021aba096bfff9bc2c69e3a195a79242a2e2842
mike <esanu@ualberta.ca>
2018-04-20 22:34:26 -0600

    adding moving average filter

--- H2-Board-Arduino-Code/H2-Board-Arduino-Code.ino
+++ H2-Board-Arduino-Code/H2-Board-Arduino-Code.ino
@@ -1,3 +1,5 @@
+#include <RingBuf.h>
+
 int H2_OK_PIN = A0; // Initialize variable for H2-board output pin
 int H2_SENSOR_PIN = A4; // Initialize variable for H2-sensor output pin
 int H2_LED1_PIN = A2; // Initialize variable for H2 indicator LED (LED1 on board--red)
@@ -8,8 +10,11 @@ int ESTOP_LED2_PIN = A3; // Initialize variable for ESTOP indicator LED (LED2 on
 
 int h2_sensor_value; // Initialize input variable for H2_SENSOR_PIN
 int h2_sensor_cutoff = 95; // Initialize variable to store cutoff value for H2-sensor
+int h2_sensor_avg;
 bool estop1_value; // Initialize input variable for ESTOP1_PIN
 bool estop2_value; // Initialize input variable for ESTOP2_PIN
+
+RingBufC buf(10*sizeof(int),10);
 
 
 void setup() {
@@ -19,17 +24,25 @@ void setup() {
   pinMode(H2_SENSOR_PIN, INPUT); // Set H2_SENSOR_PIN to input
   pinMode(ESTOP1_PIN, INPUT); // Set ESTOP1_PIN to input
   pinMode(ESTOP2_PIN, INPUT); // Set ESTOP2_PIN to input
-
-  Serial.begin(9600); // Begin serial monitor output -- for testing and setting a value for h2_sensor_cutoff (remove later)
+  
+  Serial.begin(115200); // Begin serial monitor output -- for testing and setting a value for h2_sensor_cutoff (remove later)
 }
 
 void loop() {
+  int garbage = 0;
   h2_sensor_value = analogRead(H2_SENSOR_PIN); // Read value from H2_SENSOR_PIN and store in h2_sensor_value
   estop1_value = digitalRead(ESTOP1_PIN); // Read value from ESTOP1_PIN and store in estop1_value
   estop2_value = digitalRead(ESTOP2_PIN); // Read value from ESTOP2_PIN and store in estop2_value
 
+  buf.add(&h2_sensor_value);
 
-  if ((estop1_value == LOW) || (estop2_value == LOW) || (h2_sensor_value >= h2_sensor_cutoff))
+  if(buf.numElements() < 10){
+    return;
+  }
+
+  h2_sensor_avg = getBufAvg(buf);
+
+  if ((estop1_value == LOW) || (estop2_value == LOW) || (h2_sensor_avg >= h2_sensor_cutoff))
   {
     digitalWrite(H2_OK_PIN, LOW); // If either ESTOP is pushed or excess hydrogen is sensed, shut down system
   }
@@ -49,7 +62,7 @@ void loop() {
 
 
 
-  if (h2_sensor_value >= h2_sensor_cutoff) // If excess hydrogen is sensed, turn on H2 indicator LED
+  if (h2_sensor_avg >= h2_sensor_cutoff) // If excess hydrogen is sensed, turn on H2 indicator LED
   {
     digitalWrite(H2_LED1_PIN, HIGH); // Turn on red LED
   }
@@ -58,9 +71,19 @@ void loop() {
     digitalWrite(H2_LED1_PIN, LOW); // If no excess hydrogen, keep red LED off
   }
 
+  buf.pull(&garbage);
 
-
-  Serial.println(h2_sensor_value); // Print read value from H2 sensor to serial monitor -- for testing and setting a suitable value for h2_sensor_cutoff (remove later)
-  delay(500); // Delay reading to make serial monitor easier to track (remove later)
+  Serial.println(h2_sensor_avg); // Print read value from H2 sensor to serial monitor -- for testing and setting a suitable value for h2_sensor_cutoff (remove later)
+  delay(50); // Delay reading to make serial monitor easier to track (remove later)
 
 }
+
+int getBufAvg(RingBufC buf){
+  int avg = 0;
+  int num_elements = buf.numElements();
+
+  for(int i=0;i<num_elements;i++){
+    avg += *((int*)buf.peek(i));
+  }
+}
+
